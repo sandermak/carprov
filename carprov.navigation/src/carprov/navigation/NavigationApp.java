845d4ce5d68645ea1999f9ae0d8dd1654e5cd5ce
Sander Mak <sander.mak@luminis.eu>
2015-01-11 15:52:14 +0100

    Add navigation picture and destination

--- carprov.navigation/src/carprov/navigation/NavigationApp.java
+++ carprov.navigation/src/carprov/navigation/NavigationApp.java
@@ -1,6 +1,18 @@
 package carprov.navigation;
 
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -35,10 +47,23 @@ public class NavigationApp implements App {
 
 	@Override
 	public Node getMainApp() {
-		Text text = new Text("NAVIGATION!!");
-		text.setFill(Color.AQUAMARINE);
-		text.setFont(new Font(20));
-		return text;
+	    
+	    Text text = new Text("Destination: ");
+	    text.setFont(new Font("Open Sans", 18));
+        text.setFill(Color.AZURE);
+        VBox box = new VBox(text);
+        box.setPadding(new Insets(2));
+        box.setBackground(new Background(new BackgroundFill(Color.web("#444444"), new CornerRadii(3), Insets.EMPTY)));
+	    TextField dest = new TextField("JFokus 2015!");
+        FlowPane flowPane = new FlowPane(box, dest);
+        flowPane.setPadding(new Insets(30));
+	    Image image = DashboardHelper.getImage(bundleContext, "map").getImage();
+	    flowPane.setBackground(
+	            new Background(
+	                    new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, 
+	                            new BackgroundSize(100, 100,true, true, false, true))));
+	    
+        return flowPane;
 	}
 
 }
